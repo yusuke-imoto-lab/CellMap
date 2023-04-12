e5ebe139ca1eff482efbc050191005be1fb8d714
yusukeimoto <85976418+yusukeimoto@users.noreply.github.com>
2023-04-12 14:02:55 +0900

    Update cellmap.py

--- cellmap/cellmap.py
+++ cellmap/cellmap.py
@@ -193,7 +193,7 @@ def Hodge_decomposition(
     rotation_vkey = 'rotation_velocity',
     graph_key = 'CM_graph',
     graph_method = 'Delauney',
-    alpha = 0.2,
+    HD_rate = 0.2,
     n_neighbors = 10,
     contribution_rate_pca = 0.95,
     cutedge_vol  = None,
@@ -251,7 +251,7 @@ def Hodge_decomposition(
         source = np.ravel(np.repeat(np.arange(exp_HD.shape[0]).reshape((-1, 1)),n_neighbors,axis=1))
         target = np.ravel(indices)
     
-    if alpha > 0:
+    if HD_rate > 0:
         idx_vel_HD = np.isnan(vel_HD[0])==False
         X1,X2 = exp_HD[:,idx_vel_HD][source],exp_HD[:,idx_vel_HD][target]
         V1,V2 = vel_HD[:,idx_vel_HD][source],vel_HD[:,idx_vel_HD][target]
@@ -264,7 +264,7 @@ def Hodge_decomposition(
     else:
         edge_vel_HD = 0
     
-    if alpha < 1:
+    if HD_rate < 1:
         idx_vel_LD = np.isnan(vel_LD[0])==False
         X1,X2 = exp_LD[:,idx_vel_LD][source],exp_LD[:,idx_vel_LD][target]
         V1,V2 = vel_LD[:,idx_vel_LD][source],vel_LD[:,idx_vel_LD][target]
@@ -284,7 +284,7 @@ def Hodge_decomposition(
     grad_mat[tuple(np.vstack((np.arange(n_edge_),target)))] = 1
     div_mat = -grad_mat.T
     lap = -np.dot(div_mat,grad_mat)
-    edge_vel = (1-alpha)*edge_vel_LD+alpha*edge_vel_HD
+    edge_vel = (1-HD_rate)*edge_vel_LD+HD_rate*edge_vel_HD
     source_term = np.dot(div_mat,edge_vel)
     lap_inv = np.linalg.pinv(lap)
     potential = np.dot(lap_inv,source_term)
@@ -559,22 +559,71 @@ def view_stream(
     potential_vkey = 'potential_velocity',
     rotation_key = 'Hodge_rotation',
     rotation_vkey = 'rotation_velocity',
+    cluster_key = 'clusters',
     density = 4,
-    alpha=0.5,
+    alpha=0.3,
+    **kwargs
+    ):
+    
+    kwargs_arg = check_arguments(adata,
+                             basis = basis,
+                             potential_key = potential_key,
+                            )
+    basis = kwargs_arg['basis']
+    basis_key = 'X_%s' % basis
+    pot_vkey_ = '%s_%s' % (potential_vkey,basis)
+    rot_vkey_ = '%s_%s' % (rotation_vkey,basis)
+    
+    fig,ax = plt.subplots(1,3,figsize=(24,8),tight_layout=True)
+    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=vkey, title='RNA velocity',ax=ax[0],color=cluster_key,
+                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2,**kwargs)
+    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=potential_vkey, title='Potential flow',ax=ax[1],color=cluster_key,
+                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2,**kwargs)
+    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=rotation_vkey, title='Rotational flow',ax=ax[2],color=cluster_key,
+                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2,**kwargs)
+
+
+def view_quiver(
+    adata,
+    basis = 'umap',
+    vkey = 'velocity',
+    potential_vkey = 'potential_velocity',
+    rotation_vkey = 'rotation_velocity',
+    cluster_key='clusters',
+    alpha=0.3,
+    fontsize = 18,
     **kwargs
     ):
     
     kwargs_arg = check_arguments(adata,basis = basis)
     basis = kwargs_arg['basis']
-    
+    basis_key = 'X_%s' % basis
+    vkey_ = '%s_%s' % (vkey,basis)
+    pot_vkey_ = '%s_%s' % (potential_vkey,basis)
+    rot_vkey_ = '%s_%s' % (rotation_vkey,basis)
+    cluster_set = np.unique(adata.obs[cluster_key].values)
+    print(cluster_set)
+    cmap = plt.get_cmap("tab20")
+    color = np.zeros(adata.shape[0],dtype=int)
+    for j in range(len(cluster_set)):
+        idx = adata.obs[cluster_key] == cluster_set[j]
+        color[idx] = j
     fig,ax = plt.subplots(1,3,figsize=(24,8),tight_layout=True)
-    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=vkey, title='RNA velocity',ax=ax[0],
-                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2)
-    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=potential_vkey, title='Potential flow',ax=ax[1],
-                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2)
-    scv.pl.velocity_embedding_stream(adata,basis=basis,vkey=rotation_vkey, title='Rotational flow',ax=ax[2],
-                                     show=False,density=density,alpha=alpha,fontsize=18,legend_fontsize=16, legend_loc=None,arrow_size=2,linewidth=2)
-
+    for i in range(3):
+        for j in range(len(cluster_set)):
+            idx = adata.obs[cluster_key] == cluster_set[j]
+            ax[i].scatter(adata.obsm[basis_key][idx,0],adata.obsm[basis_key][idx,1],s=200,alpha=alpha,label=cluster_set[j],color=cmap(j),zorder=0)
+            ax[i].text(np.mean(adata.obsm[basis_key][idx,0]),np.mean(adata.obsm[basis_key][idx,1]),cluster_set[j],fontsize=fontsize,
+                       ha='center',va='center',weight='bold')
+    ax[0].quiver(adata.obsm[basis_key][:,0],adata.obsm[basis_key][:,1],adata.obsm[vkey_][:,0],adata.obsm[vkey_][:,1],scale=1,zorder=1)
+    ax[0].set_title('RNA velocity',fontsize=fontsize)
+    ax[0].axis('off')
+    ax[1].quiver(adata.obsm[basis_key][:,0],adata.obsm[basis_key][:,1],adata.obsm[pot_vkey_][:,0],adata.obsm[pot_vkey_][:,1],scale=1,zorder=1,**kwargs)
+    ax[1].set_title('Potential flow',fontsize=fontsize)
+    ax[1].axis('off')
+    ax[2].quiver(adata.obsm[basis_key][:,0],adata.obsm[basis_key][:,1],adata.obsm[rot_vkey_][:,0],adata.obsm[rot_vkey_][:,1],scale=1,zorder=1,**kwargs)
+    ax[2].set_title('Rotational flow',fontsize=fontsize)
+    ax[2].axis('off')
 
 def view_surface_genes(
     adata,
