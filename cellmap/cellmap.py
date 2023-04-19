a4f7f97b7c45d99119a0764d1ecd3a1a341efe74
yusukeimoto <85976418+yusukeimoto@users.noreply.github.com>
2023-04-19 10:54:32 +0900

    Update cellmap.py

--- cellmap/cellmap.py
+++ cellmap/cellmap.py
@@ -9,6 +9,8 @@ import networkx as nx
 import scipy
 import sklearn.preprocessing
 import sklearn.neighbors
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.colors
 import pandas as pd
 import logging
@@ -1268,11 +1270,11 @@ def view_trajectory(
     potential_key = 'potential',
     cluster_key = 'clusters',
     graph_method = 'Delauney',
+    path_key = 'path',
     n_neighbors = 10,
     contribution_rate_pca = 0.95,
     cutedge_vol  = None,
     cutedge_length = None,
-    return_path = False
 ):
 
     # kwargs_arg = check_arguments(adata, verbose = True, exp_key=exp_key, vkey = vkey, basis=basis, graph_method=graph_method)
@@ -1312,10 +1314,10 @@ def view_trajectory(
     ax.triplot(tri_,color='gray',zorder=0,alpha=0.2,lw=1)
     clusters_ = adata.obs[cluster_key]
     idx_ = clusters_ == source_cluster
-    ax.scatter(data_pos[idx_,0],data_pos[idx_,1],color=cmap_(0),zorder=10,marker='o',alpha=0.2,s=5,label=source_cluster+' (source)')
+    ax.scatter(data_pos[idx_,0],data_pos[idx_,1],color='gray',zorder=10,marker='D',alpha=0.2,s=5,label=source_cluster+' (source)')
     for i_trg_ in range(len(target_clusters)):
         idx_ = clusters_ == target_clusters[i_trg_]
-        ax.scatter(data_pos[idx_,0],data_pos[idx_,1],color=cmap_(i_trg_+1),zorder=10,marker='o',alpha=0.2,s=5,label=target_clusters[i_trg_]+' (target)')
+        ax.scatter(data_pos[idx_,0],data_pos[idx_,1],color=cmap_(i_trg_),zorder=10,marker='o',alpha=0.2,s=5,label=target_clusters[i_trg_]+' (target)')
     leg = ax.legend(bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0, fontsize=12,markerscale=3)
     for lh in leg.legendHandles: lh.set_alpha(1)
 
@@ -1345,11 +1347,51 @@ def view_trajectory(
             weights.append((sum([G[path[i]][path[i+1]]['weight'] for i in range(len(path)-1)]))/sum([np.linalg.norm(data_pos[path[i]]-data_pos[path[i+1]]) for i in range(len(path)-1)]))
             dists.append(sum([np.linalg.norm(data_pos[path[i]]-data_pos[path[i+1]]) for i in range(len(path)-1)]))
         path_all.append(pathes)
-        ax.scatter(data_pos[src_set_,0],data_pos[src_set_,1],color=cmap_(0),zorder=20,marker='o',s=20)
-        ax.scatter(data_pos[trg_set_,0],data_pos[trg_set_,1],color=cmap_(i_trg_+1),zorder=20,marker='o',s=20)
+        ax.scatter(data_pos[trg_set_,0],data_pos[trg_set_,1],color=cmap_(i_trg_),zorder=20,marker='o',s=20)
         for i in range(n_cells_):
-            src_,trg_ = src_set_[i],trg_set_[i]
-            for s,t in edges[i]:
-                ax.plot([data_pos[s,0],data_pos[t,0]],[data_pos[s,1],data_pos[t,1]],color=cmap_(i_trg_+1),zorder=10)
+            ax.plot(data_pos[pathes[i],0],data_pos[pathes[i],1],color=cmap_(i_trg_),zorder=10,ls=':')
+    ax.scatter(data_pos[src_set_,0],data_pos[src_set_,1],color='gray',zorder=20,marker='D',s=30)
     ax.axis('off')
-    if return_path: return path_all
+    adata.uns[path_key] = path_all
+
+def path_gene_profile(
+    adata,
+    source_cluster,
+    target_clusters,
+    genes,
+    path_key = 'path',
+    exp_key = None,
+    fontsize_title = 16,
+    fontsize_label = 14,
+    fontsize_legend = 12,
+):
+    if exp_key == None:
+        if scipy.sparse.issparse(adata.X): data_exp = adata.X.toarray()
+        else: data_exp = adata.X
+    else:
+        data_exp = adata.layers[exp_key]
+    path = adata.uns[path_key]
+    cmap_ = plt.get_cmap("tab10")
+
+    for gene in genes:
+        if gene in adata.var.index:
+            fig = plt.figure(figsize=(10,6))
+            for i in range(len(path)):
+                x_data,y_data = np.empty(0,dtype=float),np.empty(0,dtype=float)
+                for pi in path[i]:
+                    x_data = np.append(x_data,np.linspace(0,1,len(pi)))
+                    y_data = np.append(y_data,data_exp[:,adata.var.index==gene][pi])
+                X = x_data[:, np.newaxis]
+                poly = PolynomialFeatures(degree=10)
+                X_poly = poly.fit_transform(X)
+                model = LinearRegression()
+                model.fit(X_poly, y_data)
+                plt.scatter(x_data, y_data,color=cmap_(i),alpha=0.05)
+                plot_x = np.linspace(0,1,100)
+                plt.plot(plot_x, model.predict(poly.fit_transform(plot_x[:, np.newaxis])),color=cmap_(i),lw=5,label=target_clusters[i])
+            plt.legend(bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0,title='Target', fontsize=fontsize_legend, title_fontsize=fontsize_legend)
+            plt.xticks([0,1],['Source\n(%s)' % source_cluster,'Target'],fontsize=fontsize_label)
+            plt.title(gene,fontsize=fontsize_title)
+            plt.show()
+        else:
+            print('Gene \"%s\" was not found' % gene)
