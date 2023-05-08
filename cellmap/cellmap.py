f999deb259ef29fbe469f334c3e26a95e155ee3d
yusukeimoto <85976418+yusukeimoto@users.noreply.github.com>
2023-05-08 14:22:43 +0900

    Update cellmap.py

--- cellmap/cellmap.py
+++ cellmap/cellmap.py
@@ -308,12 +308,6 @@ def Hodge_decomposition(
     if graph_method == 'Delauney':
         source, target, idx_bd_ = create_graph(exp_LD,cutedge_vol=cutedge_vol,cutedge_length=cutedge_length,cut_std=cut_std,return_type='edges_bd')
     elif graph_method == 'knn':
-        # pca = sklearn.decomposition.PCA()
-        # exp_HD_pca = pca.fit_transform(exp_HD)
-        # n_pca = np.min(np.arange(len(pca.explained_variance_ratio_))[np.cumsum(pca.explained_variance_ratio_)>contribution_rate_pca])
-        # knn = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors+1, algorithm='kd_tree')
-        # knn.fit(exp_HD_pca[:,:n_pca])
-        # distances, indices = knn.kneighbors(exp_HD_pca[:,:n_pca])
         knn = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors+1, algorithm='kd_tree')
         knn.fit(exp_LD)
         distances, indices = knn.kneighbors(exp_LD)
@@ -350,49 +344,55 @@ def Hodge_decomposition(
     # Compute potential & rotational flow
     vel_potential = np.zeros([adata.shape[0],2],dtype=float)
     vel_rotation = np.zeros([adata.shape[0],2],dtype=float)
-    src_trg_ = np.hstack((source,target))
-    trg_src_ = np.hstack((target,source))
-    pot_flow_2_ = np.hstack((pot_flow_,-pot_flow_))
-    rot_flow_2_ = np.hstack((rot_flow_,-rot_flow_))
     edge_vel_norm = np.linalg.norm(edge_velocity(exp_LD,vel_LD,source,target,normalization=False))
-    # if graph_method == 'Delauney':
-    for i in range(adata.shape[0]):
-        idx_ = src_trg_ == i
-        # ex_s = -(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]])/np.linalg.norm(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]],ord=2)
-        # vel_potential[i] = 2.*edge_vel_norm*np.mean(pot_flow_2_[idx_]*ex_s.T,axis=1)
-        # vel_rotation[i]  = 2.*edge_vel_norm*np.mean(rot_flow_2_[idx_]*ex_s.T,axis=1)
-        dis_ = np.linalg.norm(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]],axis=1,ord=2)
-        dis_[dis_==0] = 1
-        ex_ = -(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]]).T/dis_
-        vel_potential[i] = 4.*edge_vel_norm*np.mean(pot_flow_2_[idx_]*ex_,axis=1)
-        vel_rotation[i]  = 4.*edge_vel_norm*np.mean(rot_flow_2_[idx_]*ex_,axis=1)
-    adata.obsm[pot_vkey_] = vel_potential
-    adata.obsm[rot_vkey_] = vel_rotation
-    # elif graph_method == 'knn':
-    #     knn = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors+1, algorithm='kd_tree')
-    #     knn.fit(exp_LD)
-    #     distances, indices = knn.kneighbors(exp_LD)
-    #     distances, indices = distances[:,1:], indices[:,1:]
-    #     for i in range(adata.shape[0]):
-    #         ex_s = (exp_LD[indices[i]]-exp_LD[i])/np.linalg.norm(exp_LD[indices[i]]-exp_LD[i],ord=2)
-    #         vel_potential[i] = -4.*edge_vel_norm*np.mean((pot_flow_[indices[i]]-pot_flow_[i])*ex_s.T,axis=1)
-    #         vel_rotation[i]  = -4.*edge_vel_norm*np.mean((rot_flow_[indices[i]]-rot_flow_[i])*ex_s.T,axis=1)
-    #     adata.obsm[pot_vkey_] = vel_potential
-    #     adata.obsm[rot_vkey_] = vel_rotation
+    if graph_method == 'Delauney':
+        src_trg_ = np.hstack((source,target))
+        trg_src_ = np.hstack((target,source))
+        pot_flow_2_ = np.hstack((pot_flow_,-pot_flow_))
+        rot_flow_2_ = np.hstack((rot_flow_,-rot_flow_))
+        for i in range(adata.shape[0]):
+            idx_ = src_trg_ == i
+            dis_ = np.linalg.norm(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]],axis=1,ord=2)
+            dis_[dis_==0] = 1
+            ex_ = -(exp_LD[src_trg_[idx_]]-exp_LD[trg_src_[idx_]]).T/dis_
+            vel_potential[i] = 4.*edge_vel_norm*np.mean(pot_flow_2_[idx_]*ex_,axis=1)
+            vel_rotation[i]  = 4.*edge_vel_norm*np.mean(rot_flow_2_[idx_]*ex_,axis=1)
+        adata.obsm[pot_vkey_] = vel_potential
+        adata.obsm[rot_vkey_] = vel_rotation
+    elif graph_method == 'knn':
+        for i in range(adata.shape[0]):
+            idx_ = source == i
+            dis_ = np.linalg.norm(exp_LD[source[idx_]]-exp_LD[target[idx_]],axis=1,ord=2)
+            dis_[dis_==0] = 1
+            ex_ = -(exp_LD[source[idx_]]-exp_LD[target[idx_]]).T/dis_
+            vel_potential[i] = 4.*edge_vel_norm*np.mean(pot_flow_[idx_]*ex_,axis=1)
+            vel_rotation[i]  = 4.*edge_vel_norm*np.mean(rot_flow_[idx_]*ex_,axis=1)
+        adata.obsm[pot_vkey_] = vel_potential
+        adata.obsm[rot_vkey_] = vel_rotation
     
-    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((vel_LD[:,1],-vel_LD[:,0])).T,source,target,normalization=False))
+    
+    # vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((vel_LD[:,1],-vel_LD[:,0])).T,source,target,normalization=False))
+    div_ = np.linalg.norm(vel_LD,axis=1)
+    div_[div_==0] = 1
+    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((vel_LD[:,1]/div_,-vel_LD[:,0]/div_)).T,source,target,normalization=False))
     source_term_ = vorticity_
     streamfunc_ = -np.dot(lap_inv_,source_term_)
     adata.obs[vor_key_] = vorticity_
     adata.obs[sl_key_]  = streamfunc_-np.min(streamfunc_)
 
-    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[pot_vkey_][:,1],-adata.obsm[pot_vkey_][:,0])).T,source,target,normalization=False))
+    # vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[pot_vkey_][:,1],-adata.obsm[pot_vkey_][:,0])).T,source,target,normalization=False))
+    div_ = np.linalg.norm(adata.obsm[pot_vkey_],axis=1)
+    div_[div_==0] = 1
+    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[pot_vkey_][:,1]/div_,-adata.obsm[pot_vkey_][:,0]/div_)).T,source,target,normalization=False))
     source_term_ = vorticity_
     streamfunc_ = -np.dot(lap_inv_,source_term_)
     adata.obs[pot_vor_key_] = vorticity_
     adata.obs[pot_sl_key_] = streamfunc_-np.min(streamfunc_)
 
-    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[rot_vkey_][:,1],-adata.obsm[rot_vkey_][:,0])).T,source,target,normalization=False))
+    # vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[rot_vkey_][:,1],-adata.obsm[rot_vkey_][:,0])).T,source,target,normalization=False))
+    div_ = np.linalg.norm(adata.obsm[rot_vkey_],axis=1)
+    div_[div_==0] = 1
+    vorticity_ = np.dot(div_mat,edge_velocity(exp_LD,np.vstack((adata.obsm[rot_vkey_][:,1]/div_,-adata.obsm[rot_vkey_][:,0]/div_)).T,source,target,normalization=False))
     source_term_ = vorticity_
     streamfunc_ = -np.dot(lap_inv_,source_term_)
     adata.obs[rot_vor_key_] = vorticity_
